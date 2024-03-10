0506f1789bc771777364f6d0998d1b3dcff73dc0
TaniKroos <tanishsaini26@gmail.com>
2024-03-10 18:40:30 +0530

    agle hafte milenge shyd wo bhi

--- url-shortner/api/routes/shorten.go
+++ url-shortner/api/routes/shorten.go
@@ -1,9 +1,14 @@
 package routes
 
 import (
+	"os"
+	"strconv"
 	"time"
 
+	"github.com/TaniKroos/url-shortner/database"
+	"github.com/TaniKroos/url-shortner/helpers"
 	"github.com/gofiber/fiber/v3"
+	"github.com/redis/go-redis/v9"
 )
 
 type request struct {
@@ -20,28 +25,43 @@ type response struct {
 	XRateLimitRest time.Duration `json:"rate_limit_reset"`
 }
 
-
 func ShortenUrl(c *fiber.Ctx) error {
 	body := new(request)
 	if err := c.BodyParser(&body); err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error":"cannot parse Json"})
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "cannot parse Json"})
 
 	}
 	// rate limiting
-	// check if input is an actual url
-	if !govalidator.IsURL(body.URL){
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error":"Invalid URL"})
+	r2 := database.CreateClient(1)
+	defer r2.Close()
+	val, err := r2.Get(database.Ctx, c.IP()).Result()
+	if err == redis.Nil {
+		_ = r2.Set(database.Ctx, c.IP, os.Getenv("API_QUOTA"), 30*60*time.Second).Err()
 
+	} else {
+		val, _ := r2.Get(database.Ctx, c.IP().Result())
+		valInt, _ := strconv.Atoi(val)
+		if valInt <= 0 {
+			limit, _ := r2.TTL(database.Ctx, c.IP()).Result()
+			return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
+				"error":           "ratelimit exceeded",
+				"rate_limit_rest": limit / time.Nanosecond / time.Minute,
+			})
+		}
+	}
+
+	// check if input is an actual url
+	if !govalidator.IsURL(body.URL) {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid URL"})
 
 	}
 	// check for domain error
-	if !=helpers.RemoveDomainError(body.URL){
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error":"You Can't hack our system :)"})
-
+	if !helpers.RemoveDomainError(body.URL) {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "You Can't hack our system :)"})
 
 	}
-	// enforcessl 
-   body.URL = helpers.EnforceHTTP(body.URL)
-
+	// enforcessl
+	body.URL = helpers.EnforceHTTP(body.URL)
+	r2.Decr(database.Ctx, c.IP())
 
 }
